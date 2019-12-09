a75e7a7a307cf3a9b3039adcdcd89c0ec9a33cfa
Jack Harris <jack@zipcoders-MacBook-Pro-49.local>
2019-12-09 16:20:40 -0500

    wrote my fifteen polls

---
+++ src/main/resources/import.sql
@@ -0,0 +1,111 @@
+ insert into poll (poll_id, question) values (1, 'What is your favorite color?');
+ insert into option (option_id, option_value, poll_id) values (1, 'Red', 1);
+ insert into option (option_id, option_value, poll_id) values (2, 'Orange', 1);
+ insert into option (option_id, option_value, poll_id) values (3, 'Yellow', 1);
+ insert into option (option_id, option_value, poll_id) values (4, 'Green', 1);
+ insert into option (option_id, option_value, poll_id) values (5, 'Blue', 1);
+ insert into option (option_id, option_value, poll_id) values (6, 'Indigo', 1);
+ insert into option (option_id, option_value, poll_id) values (7, 'Violet', 1);
+
+ insert into poll (poll_id, question) values (2, 'What is the best Final Fantasy Game?');
+ insert into option (option_id, option_value, poll_id) values (8, 'Final Fantasy VI', 2);
+ insert into option (option_id, option_value, poll_id) values (9, 'Final Fantasy VII', 2);
+ insert into option (option_id, option_value, poll_id) values (10, 'Final Fantasy IX', 2);
+ insert into option (option_id, option_value, poll_id) values (11, 'Final Fantasy Tactics', 2);
+
+insert into poll (poll_id, question) values (3, 'Who is the best choice for Cloud?');
+ insert into option (option_id, option_value, poll_id) values (12, 'Tifa', 3);
+ insert into option (option_id, option_value, poll_id) values (13, 'Yuffie', 3);
+ insert into option (option_id, option_value, poll_id) values (14, 'Aerith', 3);
+
+insert into poll (poll_id, question) values (4, 'Who is your favorite Chrono Trigger Character?');
+ insert into option (option_id, option_value, poll_id) values (15, 'Chrono', 4);
+ insert into option (option_id, option_value, poll_id) values (16, 'Marle', 4);
+ insert into option (option_id, option_value, poll_id) values (17, 'Lucy', 4);
+ insert into option (option_id, option_value, poll_id) values (18, 'Robo', 4);
+ insert into option (option_id, option_value, poll_id) values (19, 'Frog', 4);
+
+insert into poll (poll_id, question) values (5, 'Who is your favorite instructor?');
+ insert into option (option_id, option_value, poll_id) values (20, 'Kris', 5);
+ insert into option (option_id, option_value, poll_id) values (21, 'Chris', 5);
+ insert into option (option_id, option_value, poll_id) values (22, 'Roberto', 5);
+ insert into option (option_id, option_value, poll_id) values (23, 'Froilan', 5);
+ insert into option (option_id, option_value, poll_id) values (24, 'Dolio', 5);
+ insert into option (option_id, option_value, poll_id) values (25, 'Dan', 5);
+ insert into option (option_id, option_value, poll_id) values (26, 'Desa', 5);
+
+insert into poll (poll_id, question) values (6, 'Who is the best person?');
+ insert into option (option_id, option_value, poll_id) values (27, 'John', 6);
+ insert into option (option_id, option_value, poll_id) values (28, 'Jane', 6);
+ insert into option (option_id, option_value, poll_id) values (29, 'They both suck', 6);
+
+insert into poll (poll_id, question) values (7, 'What is the best truck?');
+ insert into option (option_id, option_value, poll_id) values (30, 'Ford', 7);
+ insert into option (option_id, option_value, poll_id) values (31, 'Chevy', 7);
+ insert into option (option_id, option_value, poll_id) values (32, 'Dodge', 7);
+ insert into option (option_id, option_value, poll_id) values (33, 'Toyota', 7);
+ insert into option (option_id, option_value, poll_id) values (34, 'Tesla', 7);
+
+insert into poll (poll_id, question) values (8, 'Dogs or cats?');
+ insert into option (option_id, option_value, poll_id) values (35, 'Dogs', 8);
+ insert into option (option_id, option_value, poll_id) values (36, 'Cats', 8);
+ insert into option (option_id, option_value, poll_id) values (37, 'puppies, but only if they are wet', 8);
+ insert into option (option_id, option_value, poll_id) values (38, 'rabbits', 8);
+
+insert into poll (poll_id, question) values (9, 'decade of birth?');
+ insert into option (option_id, option_value, poll_id) values (39, '2010s', 9);
+ insert into option (option_id, option_value, poll_id) values (40, '2000s', 9);
+ insert into option (option_id, option_value, poll_id) values (41, '1990s', 9);
+ insert into option (option_id, option_value, poll_id) values (42, '1980s', 9);
+ insert into option (option_id, option_value, poll_id) values (43, '1970s', 9);
+ insert into option (option_id, option_value, poll_id) values (44, '1960s', 9);
+ insert into option (option_id, option_value, poll_id) values (45, 'nope', 9);
+
+insert into poll (poll_id, question) values (10, 'how sarcastic can these questions be?');
+ insert into option (option_id, option_value, poll_id) values (46, 'not at all sarcastic', 10);
+ insert into option (option_id, option_value, poll_id) values (47, 'maybe a little sarcastic', 10);
+ insert into option (option_id, option_value, poll_id) values (48, 'moderately sarcastic', 10);
+ insert into option (option_id, option_value, poll_id) values (49, 'pretty sarcastic', 10);
+ insert into option (option_id, option_value, poll_id) values (50, 'very sarcastic', 10);
+
+insert into poll (poll_id, question) values (11, 'Where is your pain?');
+ insert into option (option_id, option_value, poll_id) values (51, 'head', 11);
+ insert into option (option_id, option_value, poll_id) values (52, 'shoulders', 11);
+ insert into option (option_id, option_value, poll_id) values (53, 'knees', 11);
+ insert into option (option_id, option_value, poll_id) values (54, 'toes', 11);
+ insert into option (option_id, option_value, poll_id) values (55, 'everywhere', 11);
+
+insert into poll (poll_id, question) values (12, 'why is your pain?');
+ insert into option (option_id, option_value, poll_id) values (56, 'physical injury', 12);
+ insert into option (option_id, option_value, poll_id) values (57, 'just a boo boo', 12);
+ insert into option (option_id, option_value, poll_id) values (58, 'mean words', 12);
+ insert into option (option_id, option_value, poll_id) values (59, 'labs', 12);
+ insert into option (option_id, option_value, poll_id) values (60, 'existential dread', 12);
+
+insert into poll (poll_id, question) values (13, 'How many cats is too many?');
+ insert into option (option_id, option_value, poll_id) values (61, '1', 13);
+ insert into option (option_id, option_value, poll_id) values (62, '2', 13);
+ insert into option (option_id, option_value, poll_id) values (63, '3', 13);
+ insert into option (option_id, option_value, poll_id) values (64, '4', 13);
+ insert into option (option_id, option_value, poll_id) values (65, '5', 13);
+ insert into option (option_id, option_value, poll_id) values (66, '6', 13);
+ insert into option (option_id, option_value, poll_id) values (67, 'I feel personally attacked by this question', 13);
+
+insert into poll (poll_id, question) values (14, 'When the elder gods conquer the world, who will you devote yourself to?');
+ insert into option (option_id, option_value, poll_id) values (68, 'Cthulhu', 14);
+ insert into option (option_id, option_value, poll_id) values (69, 'Yog Sothoth', 14);
+ insert into option (option_id, option_value, poll_id) values (70, 'Nyarlathotep', 14);
+ insert into option (option_id, option_value, poll_id) values (71, 'The King in Yellow', 14);
+ insert into option (option_id, option_value, poll_id) values (72, 'Shub Niggurath', 14);
+ insert into option (option_id, option_value, poll_id) values (73, 'Nodens', 14);
+ insert into option (option_id, option_value, poll_id) values (74, 'Hastu....... you almost got me there', 14);
+
+insert into poll (poll_id, question) values (15, 'Rost me');
+ insert into option (option_id, option_value, poll_id) values (75, 'you mis-spelled roast', 15);
+ insert into option (option_id, option_value, poll_id) values (76, 'you are the kind of guy who wants to thank the chef at olive garden', 15);
+ insert into option (option_id, option_value, poll_id) values (77, 'you look like a muppet with scoliosis', 15);
+ insert into option (option_id, option_value, poll_id) values (78, 'people will follow you anywhere, if only out of a morbid sense of curiosity', 15);
+ insert into option (option_id, option_value, poll_id) values (79, 'you have delusions of adequacy', 15);
+ insert into option (option_id, option_value, poll_id) values (80, 'you must have gotten into the gene pool when the life guard was not watching', 15);
+ insert into option (option_id, option_value, poll_id) values (81, 'your face looks like it caught fire and someone tried to put it out with a wet chain', 15);
+
