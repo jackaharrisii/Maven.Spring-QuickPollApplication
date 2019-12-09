28baaeee9b9194e2d3146f748161f855073574a3
Jack Harris <jack@zipcoders-MacBook-Pro-49.local>
2019-12-09 11:04:51 -0500

    finished part 5

--- src/main/java/io/zipcoder/tc_spring_poll_application/exception/RestExceptionaHandler.java
+++ src/main/java/io/zipcoder/tc_spring_poll_application/exception/RestExceptionaHandler.java
@@ -1,8 +1,12 @@
 package io.zipcoder.tc_spring_poll_application.exception;
 
 import io.zipcoder.tc_spring_poll_application.dtos.error.ErrorDetail;
+import io.zipcoder.tc_spring_poll_application.dtos.error.ValidationError;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,10 +14,15 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import javax.servlet.http.HttpServletRequest;
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @ControllerAdvice
 public class RestExceptionaHandler {
+
+    @Autowired
+    MessageSource messageSource;
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<?> handleResourceNotFoundException(ResourceNotFoundException rnfe, HttpServletRequest request) {
@@ -32,7 +41,23 @@ public class RestExceptionaHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<?>
     handleValidationError(  MethodArgumentNotValidException manve, HttpServletRequest request){
-        ...
+        ErrorDetail errorDetail = new ErrorDetail();
+        errorDetail.getErrors();
+
+        List<FieldError> fieldErrors =  manve.getBindingResult().getFieldErrors();
+        for(FieldError fe : fieldErrors) {
+
+            List<ValidationError> validationErrorList = errorDetail.getErrors().get(fe.getField());
+            if(validationErrorList == null) {
+                validationErrorList = new ArrayList<>();
+                errorDetail.getErrors().put(fe.getField(), validationErrorList);
+            }
+            ValidationError validationError = new ValidationError();
+            validationError.setCode(fe.getCode());
+            validationError.setMessage(messageSource.getMessage(fe, null));
+            validationErrorList.add(validationError);
+        }
+        return new ResponseEntity<>(errorDetail, HttpStatus.BAD_REQUEST);
     }
 
 }
